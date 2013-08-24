2e93d64dd9bf618064769c086ebe36bfee3fa5f6
Jesús Herrera <jesusmaherrera@gmail.com>
2013-08-24 09:20:40 -0600

    se quitaron espacios inesesarios en codifgo

--- multipledb/Apps/main/views.py
+++ multipledb/Apps/main/views.py
@@ -134,7 +134,7 @@ def ArticuloManageView(request, id =None, template_name='articulo.html'):
  
     #Si estamos mandando un formulario a la vista
     if request.method =='POST':
-        articulo_form = articulos_form(request.POST, instance=  articulo)
+        articulo_form = articulos_form(request.POST, instance=articulo)
         
         precio_articulo_form = precios_articulos_form(request.POST, instance=precio_articulo)
         impuesto_articulo_form = impuestos_articulos_form(request.POST, instance= impuesto_articulo)
@@ -144,7 +144,7 @@ def ArticuloManageView(request, id =None, template_name='articulo.html'):
         #Si los datos de los formularios son correctos
         if articulo_form.is_valid() and precio_articulo_form.is_valid() and impuesto_articulo_form.is_valid() and nivel_articulo_form.is_valid() and clave_articulo_form.is_valid():
 
-            articulo = articulo_form.save(commit = False)
+            articulo = articulo_form.save(commit=False)
             
             #Para sacar nombre y clave anteriores
             nombre_old = None
@@ -167,7 +167,7 @@ def ArticuloManageView(request, id =None, template_name='articulo.html'):
 
 
 
-                precio_articulo_1 = precio_articulo_form.save(commit = False)
+                precio_articulo_1 = precio_articulo_form.save(commit=False)
                 if precio_articulo_1.id == None:
                     precio_articulo_1.id = -1
                     precio_articulo_1.articulo = articulo
@@ -175,7 +175,7 @@ def ArticuloManageView(request, id =None, template_name='articulo.html'):
                 precio_articulo_1.save()
                 
                 #Para agregar un segundo precio_articulo en ceros solo al crear uno nuevo          
-                precio_articulo_2 = precio_articulo_form.save(commit = False)
+                precio_articulo_2 = precio_articulo_form.save(commit=False)
                 if precio_articulo_2.id == -1:
                     precio_articulo_2.articulo = articulo
                     precio_articulo_2.precio_empresa = precios_empresa.objects.get(pk=PRECIOS_EMPRESA_EXTRA['default'])
@@ -183,17 +183,17 @@ def ArticuloManageView(request, id =None, template_name='articulo.html'):
                     precio_articulo_2.moneda = precio_articulo_1.moneda
                     precio_articulo_2.save()    
 
-                impuesto_articulo = impuesto_articulo_form.save(commit = False)
+                impuesto_articulo = impuesto_articulo_form.save(commit=False)
                 if impuesto_articulo.id == None:
                     impuesto_articulo.id = -1
                     impuesto_articulo.articulo = articulo
                 
-                clave_articulo = clave_articulo_form.save(commit = False)
+                clave_articulo = clave_articulo_form.save(commit=False)
                 if clave_articulo.id == None:
                     clave_articulo.id = -1
                     clave_articulo.articulo = articulo
                 
-                nivel_articulo = nivel_articulo_form.save(commit = False)
+                nivel_articulo = nivel_articulo_form.save(commit=False)
                 if nivel_articulo.id == None:
                     nivel_articulo.id = -1
                     nivel_articulo.articulo = articulo
@@ -213,7 +213,7 @@ def ArticuloManageView(request, id =None, template_name='articulo.html'):
                 nivel_articulo.save()
                 clave_articulo.save()
                 for database_x in app_databases:
-                    linea_articulo_x = LineaArticulos.objects.using(database_x).get(nombre = articulo.linea.nombre)
+                    linea_articulo_x = LineaArticulos.objects.using(database_x).get(nombre=articulo.linea.nombre)
                     #Si ya existe un articulo en esta base de datos lo modifica
                     try:
                         articulo_x = ClavesArticulos.objects.using(database_x).filter(clave=clave_articulo.clave)[0].articulo
@@ -247,11 +247,11 @@ def ArticuloManageView(request, id =None, template_name='articulo.html'):
                     #########################################################
 
                     #Precios
-                    moneda_x = Moneda.objects.using(database_x).get(nombre = precio_articulo_form.cleaned_data['moneda'].nombre)
+                    moneda_x = Moneda.objects.using(database_x).get(nombre=precio_articulo_form.cleaned_data['moneda'].nombre)
                     
-                    precio_empresa_x = precios_empresa.objects.using(database_x).get(nombre = precio_articulo_form.cleaned_data['precio_empresa'].nombre)
+                    precio_empresa_x = precios_empresa.objects.using(database_x).get(nombre=precio_articulo_form.cleaned_data['precio_empresa'].nombre)
                 
-                    precios_articulo_x = precios_articulos.objects.using(database_x).filter(articulo = articulo_x)
+                    precios_articulo_x = precios_articulos.objects.using(database_x).filter(articulo=articulo_x)
                     #Si precios_articulo_x tiene al menos un precio
                     if precios_articulo_x.count() > 0:
                         precio_articulo_x = precios_articulo_x[0]
@@ -260,8 +260,8 @@ def ArticuloManageView(request, id =None, template_name='articulo.html'):
                         precio_articulo_x.precio = precio_articulo_form.cleaned_data['precio']
                         precio_articulo_x.save(using=database_x)
                     else:
-                        precios_articulos.objects.using(database_x).create(id = c_get_next_key(database_x), articulo = articulo_x, moneda = moneda_x, precio_empresa = precio_empresa_x, precio = precio_articulo_form.cleaned_data['precio'])
-                        precios_articulos.objects.using(database_x).create(id = c_get_next_key(database_x), articulo = articulo_x, moneda = moneda_x, precio_empresa = precios_empresa.objects.using(database_x).get(pk=PRECIOS_EMPRESA_EXTRA[database_x]) , precio = 0)
+                        precios_articulos.objects.using(database_x).create(id=c_get_next_key(database_x), articulo=articulo_x, moneda=moneda_x, precio_empresa=precio_empresa_x, precio=precio_articulo_form.cleaned_data['precio'])
+                        precios_articulos.objects.using(database_x).create(id=c_get_next_key(database_x), articulo=articulo_x, moneda=moneda_x, precio_empresa=precios_empresa.objects.using(database_x).get(pk=PRECIOS_EMPRESA_EXTRA[database_x]) , precio=0)
                 
                     #Impuestos
                     impuesto_x = Impuesto.objects.using(database_x).get(nombre = impuesto_articulo.impuesto.nombre)
@@ -272,11 +272,11 @@ def ArticuloManageView(request, id =None, template_name='articulo.html'):
                         impuesto_articulo_x.impuesto = impuesto_x
                         impuesto_articulo_x.save(using=database_x)
                     else:
-                        ImpuestosArticulo.objects.using(database_x).create(id = -1, articulo = articulo_x, impuesto = impuesto_x)
+                        ImpuestosArticulo.objects.using(database_x).create(id=-1, articulo=articulo_x, impuesto=impuesto_x)
                 
                     #clave
-                    rol_x = RolesClavesArticulos.objects.using(database_x).get(nombre = clave_articulo.rol.nombre)
-                    claves_articulo_x = ClavesArticulos.objects.using(database_x).filter(articulo = articulo_x)
+                    rol_x = RolesClavesArticulos.objects.using(database_x).get(nombre=clave_articulo.rol.nombre)
+                    claves_articulo_x = ClavesArticulos.objects.using(database_x).filter(articulo=articulo_x)
 
                     if claves_articulo_x.count() > 0:
                         clave_articulo_x = claves_articulo_x[0]
@@ -284,25 +284,25 @@ def ArticuloManageView(request, id =None, template_name='articulo.html'):
                         clave_articulo_x.clave = clave_articulo.clave
                         clave_articulo_x.save(using=database_x)
                     else:
-                        ClavesArticulos.objects.using(database_x).create(id = -1, articulo = articulo_x, rol = rol_x, clave = clave_articulo.clave)
+                        ClavesArticulos.objects.using(database_x).create(id=-1, articulo=articulo_x, rol=rol_x, clave=clave_articulo.clave)
                 
                     #Puntos de reorden
-                    almacen_x = Almacenes.objects.using(database_x).get(nombre = nivel_articulo.almacen.nombre)
-                    niveles_articulo_x = NivelesArticulos.objects.using(database_x).filter(articulo = articulo_x)
+                    almacen_x = Almacenes.objects.using(database_x).get(nombre=nivel_articulo.almacen.nombre)
+                    niveles_articulo_x = NivelesArticulos.objects.using(database_x).filter(articulo=articulo_x)
                     if niveles_articulo_x.count() <= 0:
-                        NivelesArticulos.objects.using(database_x).create(id = -1, articulo = articulo_x, almacen =almacen_x, 
-                            localizacion = nivel_articulo.localizacion, inventario_maximo = nivel_articulo.inventario_maximo, inventario_minimo = nivel_articulo.inventario_minimo,
-                            punto_reorden = nivel_articulo.punto_reorden )
+                        NivelesArticulos.objects.using(database_x).create(id=-1, articulo=articulo_x, almacen=almacen_x, 
+                            localizacion=nivel_articulo.localizacion, inventario_maximo=nivel_articulo.inventario_maximo, inventario_minimo=nivel_articulo.inventario_minimo,
+                            punto_reorden=nivel_articulo.punto_reorden)
                     
                 return HttpResponseRedirect('/articulos/')
     #Si estamos cargando la vista por primera ves
     else:
-        articulo_form = articulos_form(instance=  articulo)
+        articulo_form = articulos_form(instance= articulo)
 
         precio_articulo_form = precios_articulos_form(instance=precio_articulo)
-        impuesto_articulo_form = impuestos_articulos_form(instance= impuesto_articulo)
+        impuesto_articulo_form = impuestos_articulos_form(instance=impuesto_articulo)
         nivel_articulo_form = niveles_articulos_form(instance=nivel_articulo)
-        clave_articulo_form = claves_articulos_form(instance = clave_articulo)
+        clave_articulo_form = claves_articulos_form(instance=clave_articulo)
       
 
     c = {
